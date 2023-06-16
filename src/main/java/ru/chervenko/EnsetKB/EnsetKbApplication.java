21c4d003597759ae7140a3d68559d817bde417aa
Alexey Chervenko <alexbusymann@gmail.com>
2023-06-16 15:49:34 +0300

    First commit

---
+++ src/main/java/ru/chervenko/EnsetKB/EnsetKbApplication.java
@@ -0,0 +1,13 @@
+package ru.chervenko.EnsetKB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsetKbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsetKbApplication.class, args);
+	}
+
+}
